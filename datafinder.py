6551fcb3ef1bd9bd4dbe43b40127435040967485
Holger Graef <holger.graef@lpa.ens.fr>
2016-03-09 14:33:21 +0100

    Added new search function to datafinder.py

--- datafinder.py
+++ datafinder.py
@@ -7,3 +7,27 @@ def findtextfiles(dirs):
         f_list.extend((glob(di + '/*.txt') + glob(di + '/*/*.txt')))
         
     return f_list
+    
+
+def findVg1sweepspectra(dir_sample):
+    '''
+        find spectra and sort file list by gate voltage
+    '''
+    f_list = (glob(dir_sample + '/*/S-parameter/*.txt') + glob(dir_sample + '/S-parameter/*.txt'))
+    
+    # helper function to enable sorting by Vg in filename
+    def sorting(name):
+        if name.find('Vg1=')!=-1:
+            vg = float(name[name.find('Vg1=')+4:
+                                    name.find('Vg1=')+12])
+        else: 
+            vg = float(name[name.find('Vgate=')+6:
+                                    name.find('Vgate=')+14])
+        if 'return' in name: vg += 1000         # shift up return sweep
+        return vg    
+    
+    # use helper function to sort file list
+    f_list = sorted(f_list, key=sorting)
+    
+    return f_list
+    
