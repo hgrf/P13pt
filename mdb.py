af7d8069f6656a2d872b1bf34b18aed668b53aad
Holger Graef <holger.graef@lpa.ens.fr>
2016-05-20 16:51:49 +0200

    Moved plotter to separate file

--- mdb.py
+++ mdb.py
@@ -10,8 +10,7 @@ from PyQt4.QtGui import (QApplication, QFileSystemModel,
                          QTextEdit, QTabWidget, QComboBox)
 from PyQt4.QtCore import QDir, Qt, pyqtSlot
 
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+from plotter import Plotter
 
 import os
 import numpy as np
@@ -160,99 +159,6 @@ def forgetroot():
     treev.setRootIndex(model.index(QDir.rootPath()))
     config.remove_option('main', 'root')
 
-class PlotterW(QWidget):
-    def __init__(self, parent=None):
-        super(PlotterW, self).__init__(parent)
-
-        self.xvar = QListWidget(self)
-        self.yvar = QListWidget(self)
-        self.selectvar = QComboBox(self)
-        self.selectval = QListWidget(self)
-
-        self.selectval.setSelectionMode(QListWidget.MultiSelection)
-
-        self.xvar.itemClicked.connect(self.plot)
-        self.yvar.itemClicked.connect(self.plot)
-        self.selectvar.activated[int].connect(self.selectedvar)
-        self.selectval.itemSelectionChanged.connect(self.reducedataset)
-
-        selectlayout = QVBoxLayout()
-        selectlayout.addWidget(self.selectvar)
-        selectlayout.addWidget(self.selectval)
-
-        configpanelayout = QHBoxLayout()
-        configpanelayout.addWidget(self.xvar)
-        configpanelayout.addWidget(self.yvar)
-        configpanelayout.addLayout(selectlayout)
-
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-
-        # set the layout
-        layout = QVBoxLayout()
-        layout.addLayout(configpanelayout)
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        self.setLayout(layout)
-
-    def selectedvar(self, i):
-        self.selectval.clear()
-        vals = np.unique(self.data[i])
-        for v in vals:
-            self.selectval.addItem(str(v))
-
-    def reducedataset(self):
-        if not len(self.selectval.selectedItems()):
-            self.reddata = self.data
-            self.plot()
-            return
-
-        selection = [False]*len(self.data[0])
-        for it in self.selectval.selectedItems():
-            selection = np.logical_or(selection, self.data[self.selectvar.currentIndex()]==float(it.text()))
-        self.reddata = self.data[:, selection]
-        self.plot(key=self.selectvar.currentIndex())
-
-    def setheader(self, header):
-        self.header = header
-        self.xvar.clear()
-        self.yvar.clear()
-        self.selectvar.clear()
-        for col in header:
-            self.xvar.addItem(col)
-            self.yvar.addItem(col)
-            self.selectvar.addItem(col)
-
-    def setdata(self, data):
-        self.data = data
-        self.reddata = data
-
-    @pyqtSlot()         # don't accept arguments (otherwise might get some
-                        # QListWidget or other stuff passed as key argument if used as slot)
-    def plot(self, key=None):
-        ix = self.xvar.currentRow()
-        iy = self.yvar.currentRow()
-        if ix < 0 or iy < 0:
-            return
-
-        ax = self.figure.add_subplot(111)
-        ax.clear()
-        #ax.hold(False)
-
-        if key is None:
-            ax.plot(self.reddata[ix], self.reddata[iy], '*-')
-        else:
-            uniquevals = np.unique(self.reddata[key])
-            for v in uniquevals:
-                ax.plot(self.reddata[ix, self.reddata[key]==v], self.reddata[iy, self.reddata[key]==v], '*-', label=str(v))
-            ax.legend()
-
-        ax.set_xlabel(self.header[ix])
-        ax.set_ylabel(self.header[iy])
-
-        self.canvas.draw()
-
 if __name__ == '__main__':
     # CD into directory where this script is saved
     d = os.path.dirname(__file__)
@@ -332,7 +238,7 @@ if __name__ == '__main__':
     infow = QTabWidget(splitter)
 
     analyserw = QTextEdit(splitter)
-    plotterw = PlotterW()
+    plotterw = Plotter()
 
     infow.addTab(analyserw, "Analyser")
     infow.addTab(plotterw, "Plotter")
---
+++ plotter.py
@@ -0,0 +1,102 @@
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+import matplotlib.pyplot as plt
+import numpy as np
+
+from PyQt4.QtGui import (QWidget, QListWidget, QComboBox, QVBoxLayout,
+                         QHBoxLayout)
+
+from PyQt4.QtCore import pyqtSlot
+
+class Plotter(QWidget):
+    def __init__(self, parent=None):
+        super(Plotter, self).__init__(parent)
+
+        self.xvar = QListWidget(self)
+        self.yvar = QListWidget(self)
+        self.selectvar = QComboBox(self)
+        self.selectval = QListWidget(self)
+
+        self.selectval.setSelectionMode(QListWidget.MultiSelection)
+
+        self.xvar.itemClicked.connect(self.plot)
+        self.yvar.itemClicked.connect(self.plot)
+        self.selectvar.activated[int].connect(self.selectedvar)
+        self.selectval.itemSelectionChanged.connect(self.reducedataset)
+
+        selectlayout = QVBoxLayout()
+        selectlayout.addWidget(self.selectvar)
+        selectlayout.addWidget(self.selectval)
+
+        configpanelayout = QHBoxLayout()
+        configpanelayout.addWidget(self.xvar)
+        configpanelayout.addWidget(self.yvar)
+        configpanelayout.addLayout(selectlayout)
+
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        # set the layout
+        layout = QVBoxLayout()
+        layout.addLayout(configpanelayout)
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+
+    def selectedvar(self, i):
+        self.selectval.clear()
+        vals = np.unique(self.data[i])
+        for v in vals:
+            self.selectval.addItem(str(v))
+
+    def reducedataset(self):
+        if not len(self.selectval.selectedItems()):
+            self.reddata = self.data
+            self.plot()
+            return
+
+        selection = [False]*len(self.data[0])
+        for it in self.selectval.selectedItems():
+            selection = np.logical_or(selection, self.data[self.selectvar.currentIndex()]==float(it.text()))
+        self.reddata = self.data[:, selection]
+        self.plot(key=self.selectvar.currentIndex())
+
+    def setheader(self, header):
+        self.header = header
+        self.xvar.clear()
+        self.yvar.clear()
+        self.selectvar.clear()
+        for col in header:
+            self.xvar.addItem(col)
+            self.yvar.addItem(col)
+            self.selectvar.addItem(col)
+
+    def setdata(self, data):
+        self.data = data
+        self.reddata = data
+
+    @pyqtSlot()         # don't accept arguments (otherwise might get some
+                        # QListWidget or other stuff passed as key argument if used as slot)
+    def plot(self, key=None):
+        ix = self.xvar.currentRow()
+        iy = self.yvar.currentRow()
+        if ix < 0 or iy < 0:
+            return
+
+        ax = self.figure.add_subplot(111)
+        ax.clear()
+        #ax.hold(False)
+
+        if key is None:
+            ax.plot(self.reddata[ix], self.reddata[iy], '*-')
+        else:
+            uniquevals = np.unique(self.reddata[key])
+            for v in uniquevals:
+                ax.plot(self.reddata[ix, self.reddata[key]==v], self.reddata[iy, self.reddata[key]==v], '*-', label=str(v))
+            ax.legend()
+
+        ax.set_xlabel(self.header[ix])
+        ax.set_ylabel(self.header[iy])
+
+        self.canvas.draw()
