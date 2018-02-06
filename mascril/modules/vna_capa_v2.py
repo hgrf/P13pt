3c2a1f34189a4c141a6e36077a04e45083444407
green-mercury <holger.graef@gmail.com>
2018-02-06 16:06:11 +0100

    MAScriL: added progress bar and made Bilt initialisation optional in vna_capa_v2

--- mascril/modules/vna_capa_v2.py
+++ mascril/modules/vna_capa_v2.py
@@ -5,6 +5,7 @@ from P13pt.drivers.anritsuvna import AnritsuVNA
 import time
 import numpy as np
 import os
+import sys
 import errno
 
 class Measurement(MeasurementBase):
@@ -13,8 +14,9 @@ class Measurement(MeasurementBase):
         'Rg': 100e3,
         'stabilise_time': 0.3,
         'comment': String(''),
-        'data_dir': Folder(r'D:\MeasurementJANIS\Holger\PRC-TopGate-D3\2018.01.18_VNA_RT'),
-        'use_vna': Boolean(True)
+        'data_dir': Folder(r'D:\MeasurementJANIS\Holger\PRC-TopGate-D3\2018.02.06_VNA_RT'),
+        'use_vna': Boolean(True),
+        'init_bilt': Boolean(False)
     }
 
     observables = ['Vg', 'Vgm', 'Ileak']
@@ -23,7 +25,7 @@ class Measurement(MeasurementBase):
         ['np.abs(Ileak) > 1e-8', MeasurementBase.ALARM_CALLCOPS]
     ]
 
-    def measure(self, data_dir, Vgs, Rg, comment, stabilise_time, use_vna, **kwargs):
+    def measure(self, data_dir, Vgs, Rg, comment, stabilise_time, use_vna, init_bilt, **kwargs):
         print "==================================="        
         print "Starting acquisition script..."
 
@@ -31,8 +33,11 @@ class Measurement(MeasurementBase):
         try:
             print "Setting up DC sources and voltmeters..."
             bilt = Bilt('TCPIP0::192.168.0.2::5025::SOCKET')
-            # source (bilt, channel, range, filter, slope in V/ms, label):
-            self.sourceVg = sourceVg = BiltVoltageSource(bilt, "I1", "12", "1", 0.005, "Vg")
+            if init_bilt:
+                # source (bilt, channel, range, filter, slope in V/ms, label):
+                self.sourceVg = sourceVg = BiltVoltageSource(bilt, "I1", "12", "1", 0.005, "Vg")
+            else:
+                self.sourceVg = sourceVg = BiltVoltageSource(bilt, "I1", initialise=False)
             # voltmeter (bilt, channel, filt, label=None)
             self.meterVg = meterVg = BiltVoltMeter(bilt, "I5;C1", "2", "Vgm")
             print "DC sources and voltmeters are set up."
@@ -43,7 +48,7 @@ class Measurement(MeasurementBase):
         try:
             print "Setting up VNA"
             vna = AnritsuVNA('GPIB::6::INSTR')
-            self.freqs = vna.get_freq_list()         # get frequency list
+            sweeptime = vna.get_sweep_time()
             print "VNA is set up."
         except:
             print "There has been an error setting up the VNA."
@@ -63,6 +68,9 @@ class Measurement(MeasurementBase):
             except OSError as e:
                 if e.errno != errno.EEXIST:
                     raise
+            if vna.get_sweep_type() == 'FSEGM':
+                with open(os.path.join(spectra_fol, 'VNAconfig'), 'w') as f:
+                    vna.dump_freq_segments(f)
 
         for Vg in Vgs:
             if self.flags['quit_requested']:
@@ -88,8 +96,26 @@ class Measurement(MeasurementBase):
 
             if use_vna:
                 # save VNA data
-                print "Getting VNA spectra..."
-                vna.single_sweep()
+                print "Getting VNA spectra|"
+                vna.single_sweep(wait=False)
+                # display sweep progress
+                # TODO: should put this in the single sweep function
+                t0 = time.time()
+                i = 0 # number of # displayed
+                while True:
+                    t1 = time.time()
+                    if t1-t0 >= sweeptime:
+                        break
+                    if (t1-t0)/sweeptime*100./5. > i+1:
+                        sys.stdout.write('#')
+                        sys.stdout.flush()
+                        i += 1
+                    time.sleep(0.01)
+                sys.stdout.write('\n')
+                sys.stdout.flush()
+                # make sure sweep is really done
+                while not vna.is_sweep_done():
+                    pass
                 table = vna.get_table([1,2,3,4])
                 timestamp = time.strftime('%Y-%m-%d_%Hh%Mm%Ss')
                 spectrum_file = timestamp+'_Vg=%2.4f'%(Vg)+'.txt'
