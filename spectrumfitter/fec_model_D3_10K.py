5b137103786ad5f6f2b2481e3012d4892e30edc4
Holger Graef <holger.graef@lpa.ens.fr>
2018-02-09 09:30:02 +0100

    Added models

---
+++ spectrumfitter/fec_model_D3_10K.py
@@ -0,0 +1,289 @@
+import numpy as np
+from lmfit import Parameters, minimize
+
+class Model:
+    # dictionary of the model's parameters
+    # for each parameter, we store the minimum and maximum value,
+    # an initial value, a multiplier value and a unit
+    params = {
+        'r': [0.1, 20000., 1000., 1, 'Ohm'],
+        'c': [0.1, 1000, 200., 1e-15, 'fF'],
+        'l': [0, 2000, 0., 1e-12, 'pH'],
+        'gl': [0, 1000, 0., 1e-6, 'uS'],
+        'ra': [0, 1000, 200., 1, 'Ohm'],
+        'ca': [0, 1000, 0., 1e-15, 'fF'],
+        'rasup': [0, 1000, 0., 1, 'Ohm']
+    }
+
+    values = {}    # this is where the fitter will store the values
+
+    def __init__(self):
+        self.reset_values()
+
+    def reset_values(self):
+        for p in self.params:
+            self.values[p] = self.params[p][2]*self.params[p][3]
+
+    def admittance(self, w, r, c, l, gl, ra, ca, rasup):
+        """Admittance of a Field Effect Capacitor
+
+        Parameters
+        ----------
+        w : float or np.array
+            Pulsation at which to compute the admitance
+
+        r : float or np.array
+            Resistance of the R-C-L line
+
+        c : float or np.array
+            Capacitance of the R-C-L line
+
+        l : float or np.array
+            Inductance of the R-C-L line
+
+        gl : float or np.array
+            Leak conductance
+
+        ra : float or np.array
+            Access resistance
+
+        ca : float or np.array
+            Access capacitance
+
+        rasup : float or np.array
+            Supplementary access resistance (e.g. ungated region)
+
+        Returns
+        -------
+        float : computed admittance
+
+        """
+        r = r + 1j*l*w
+        k = np.sqrt(-1j*r*c*w)
+        tlm = (1j*k/r) * np.tanh(1j*k) + gl
+        if ra == 0.:
+            adm_a = 1j*w*ca
+        else:
+            adm_a = 1j*w*ca + 1./ra
+        if ra == 0. and ca == 0.:
+            adm = 1./(1./tlm + rasup)
+        else:
+            adm = 1./(1./tlm + 1./adm_a + rasup)
+        return adm
+
+    def objective(self, params, x_data, y_data):
+        """Error function function minimized during the fitting procedure.
+
+        We perform the optimization on both the real and imaginary part.
+        """
+        r, c, l, gl, ra, ca, rasup = (params['r'].value, params['c'].value,
+                                      params['l'].value, params['gl'].value,
+                                      params['ra'].value, params['ca'].value,
+                                      params['rasup'].value)
+        computed_admittance = self.admittance(x_data, r, c, l, gl, ra, ca, rasup)
+        np.nan_to_num(computed_admittance)
+        res = np.empty((2, len(y_data)))
+        res[0] = y_data.real - computed_admittance.real
+        res[1] = y_data.imag - computed_admittance.imag
+        return np.ravel(res)
+
+    def fit_default(self, f, y, checkboxes):
+        w = 2.*np.pi*f
+
+        # create fit parameters
+        params = Parameters()
+        params.add('r', value=self.values['r'], min=1, max=120e3, vary=checkboxes['r'].isChecked())
+        params.add('c', value=self.values['c'], min=1e-15, max=1e-12, vary=checkboxes['c'].isChecked())
+        params.add('l', value=self.values['l'], min=0., max=1e-6, vary=checkboxes['l'].isChecked())
+        params.add('ra', value=self.values['ra'], min=10, max=10e3, vary=checkboxes['ra'].isChecked())
+        params.add('rasup', value=self.values['rasup'], min=0., max=10e3, vary=checkboxes['rasup'].isChecked())
+        params.add('ca', value=self.values['ca'], min=0., max=10e3, vary=checkboxes['ca'].isChecked())
+        params.add('gl', value=self.values['gl'], min=1e-5, max=1e-3, vary=checkboxes['gl'].isChecked())
+
+        # execute fit
+        res = minimize(self.objective, params, args=(w, y))
+        for p in self.values:
+            self.values[p] = res.params[p].value
+
+    def fit_stepbystep(self, base_f, base_y):
+        # define masks
+        masks = [base_f < 1e9]
+        masks += [base_f < 40e9]
+
+        # fit
+        for i, mask in enumerate(masks):
+            w = 2.*np.pi*base_f[mask]
+            y = base_y[mask]  # minus sign because Y12 and not Y11
+
+            # create fit parameters
+            params = Parameters()
+            params.add('r', value=self.values['r'], min=1, max=120e3, vary=True if i in [0] else False)
+            params.add('c', value=self.values['c'], min=1e-15, max=1e-12, vary=True if i in [0] else False)
+            params.add('l', value=self.values['l'], min=0., max=1e-6, vary=True if i in [] else False)
+            params.add('ra', value=self.values['ra'], min=10, max=10e3, vary=True if i in [] else False)
+            params.add('rasup', value=self.values['rasup'], min=0., max=10e3, vary=True if i in [] else False)
+            params.add('ca', value=self.values['ca'], min=10e-15, max=10e3, vary=True if i in [] else False)
+            params.add('gl', value=self.values['gl'], min=1e-5, max=1e-3, vary=True if i in [] else False)
+
+            # execute fit
+            res = minimize(self.objective, params, args=(w, y))
+            for p in self.values:
+                self.values[p] = res.params[p].value
+
+    def fit_firstguess(self, f, y):
+        # get leak conductance from the real part of the low frequency admittance
+        gl = np.mean(y[f<2e7].real)
+
+        # get crossover frequency
+        mask = f > 3e8  # avoid detecting the crossover associated with the leak
+        fc = f[mask][np.argmin(np.abs(y[mask].real - y[mask].imag))]
+        if fc < 3.1e8: fc = 1e9
+
+        # get capacitance
+        mask = f < 3e8
+        c = np.mean(y[mask].imag/(2.*np.pi*f[mask]))
+        r = 1./(2.*np.pi*fc*c)
+
+        self.values['gl'] = gl
+        self.values['c'] = c
+        self.values['r'] = r
+
+    def fit_RCRa(self, base_f, base_y):
+        # define masks
+        self.values['r'] = 1000.
+        self.values['c'] = 200e-15
+        self.values['ra'] = 200.
+        
+        # define masks
+        masks = [base_f < 0.2e9]    # here we will only fit C
+        masks += [base_f < 10e9]   # here we will only fit R
+        
+        # do it again
+        masks += [base_f < 0.2e9]
+        masks += [base_f < 10e9]
+        
+        # now with Ra
+        masks += [base_f < 10e9]
+
+        # fit
+        for i, mask in enumerate(masks):
+            w = 2.*np.pi*base_f[mask]
+            y = base_y[mask]  # minus sign because Y12 and not Y11
+
+            # create fit parameters
+            params = Parameters()
+            params.add('c', value=self.values['c'], min=1e-15, max=1e-12, vary=True if i in [0, 2] else False)
+            params.add('r', value=self.values['r'], min=1, max=100e3, vary=True if i in [1, 3, 4] else False)
+            params.add('ra', value=self.values['ra'], min=1, max=10e3, vary=True if i in [4] else False)
+
+            # don't fit the following params
+            # TODO: could generate these automatically
+            params.add('l', value=self.values['l'], min=0., max=1e-6, vary=False)
+            params.add('gl', value=self.values['gl'], min=0., max=1000e-6, vary=False)
+            params.add('ca', value=self.values['ca'], min=0., max=1e-12, vary=False)
+            params.add('rasup', value=self.values['rasup'], min=0., max=10e3, vary=False)
+
+            # execute fit
+            res = minimize(self.objective, params, args=(w, y))
+            for p in self.values:
+                self.values[p] = res.params[p].value
+                
+    def fit_RC(self, base_f, base_y):       
+        # define initial values
+        self.values['r'] = 1000.
+        self.values['c'] = 200e-15
+        self.values['ra'] = 200.
+        
+        # define masks
+        masks = [base_f < 0.2e9]    # here we will only fit C
+        masks += [base_f < 10e9]   # here we will only fit R
+        
+        # do it again
+        masks += [base_f < 0.2e9]
+        masks += [base_f < 10e9]
+
+        # fit
+        for i, mask in enumerate(masks):
+            w = 2.*np.pi*base_f[mask]
+            y = base_y[mask]  # minus sign because Y12 and not Y11
+
+            # create fit parameters
+            params = Parameters()
+            params.add('c', value=self.values['c'], min=1e-15, max=1e-12, vary=True if i in [0,2] else False)
+            params.add('r', value=self.values['r'], min=1, max=100e3, vary=True if i in [1,3] else False)
+        
+            # don't fit the following params
+            # TODO: could generate these automatically
+            params.add('ra', value=self.values['ra'], min=1, max=10e3, vary=False)
+            params.add('l', value=self.values['l'], min=0., max=1e-6, vary=False)
+            params.add('gl', value=self.values['gl'], min=0., max=1000e-6, vary=False)
+            params.add('ca', value=self.values['ca'], min=0., max=1e-12, vary=False)
+            params.add('rasup', value=self.values['rasup'], min=0., max=10e3, vary=False)
+
+            # execute fit
+            res = minimize(self.objective, params, args=(w, y))
+            for p in self.values:
+                self.values[p] = res.params[p].value
+                
+    def fit_RCRa_2fc(self, base_f, base_y):
+        # get crossover frequency
+        mask = base_f > 3e8  # avoid detecting the crossover associated with the leak
+        fc = base_f[mask][np.argmin(np.abs(base_y[mask].real - base_y[mask].imag))]
+        if fc < 3.1e8: fc = 1e9
+        
+        # define masks
+        masks = [base_f < fc]
+        masks += [base_f < fc*2]
+
+        # fit
+        for i, mask in enumerate(masks):
+            w = 2.*np.pi*base_f[mask]
+            y = base_y[mask]  # minus sign because Y12 and not Y11
+
+            # create fit parameters
+            params = Parameters()
+            params.add('r', value=self.values['r'], min=100, max=100e3, vary=True if i in [0, 1] else False)
+            params.add('c', value=self.values['c'], min=1e-15, max=1e-12, vary=True if i in [0, 1] else False)
+            params.add('ra', value=self.values['ra'], min=1, max=10e3, vary=True if i in [1] else False)
+
+            # don't fit the following params
+            # TODO: could generate these automatically
+            params.add('l', value=self.values['l'], min=0., max=1e-6, vary=False)
+            params.add('gl', value=self.values['gl'], min=0., max=1000e-6, vary=False)
+            params.add('ca', value=self.values['ca'], min=0., max=1e-12, vary=False)
+            params.add('rasup', value=self.values['rasup'], min=0., max=10e3, vary=False)
+
+            # execute fit
+            res = minimize(self.objective, params, args=(w, y))
+            for p in self.values:
+                self.values[p] = res.params[p].value
+
+    def fit_RLCRa(self, base_f, base_y):
+        # define masks
+        masks = [base_f < 1e9]
+        masks += [base_f < 5e9]
+        masks += [base_f < 5e9]
+        masks += [base_f < 30e9]
+
+        for i, mask in enumerate(masks):
+            w = 2.*np.pi*base_f[mask]
+            y = base_y[mask]  # minus sign because Y12 and not Y11
+
+            # create fit parameters
+            params = Parameters()
+            params.add('r', value=self.values['r'], min=100, max=100e3, vary=True if i in [0, 1] else False)
+            params.add('l', value=self.values['l'], min=0., max=1e-6, vary=True if i in [2, 3] else False)
+            params.add('c', value=self.values['c'], min=1e-15, max=1e-12, vary=True if i in [0, 1] else False)
+            params.add('ra', value=self.values['ra'], min=1, max=10e3, vary=True if i in [1, 2, 3] else False)
+
+            # don't fit the following params
+            # TODO: could generate these automatically
+            params.add('gl', value=self.values['gl'], min=0., max=1000e-6, vary=False)
+            params.add('ca', value=self.values['ca'], min=0., max=1e-12, vary=False)
+            params.add('rasup', value=self.values['rasup'], min=0., max=10e3, vary=False)
+
+            # execute fit
+            res = minimize(self.objective, params, args=(w, y))
+            for p in self.values:
+                self.values[p] = res.params[p].value
+
---
+++ spectrumfitter/fec_model_D3_10K_intelliparms.py
@@ -0,0 +1,128 @@
+import numpy as np
+from lmfit import Parameters, minimize
+
+class Model:
+    # dictionary of the model's parameters
+    # for each parameter, we store the minimum and maximum value,
+    # an initial value, a multiplier value and a unit
+#    params = {
+#        'r': [1., 1000., 1000., 1, 'Ohm'],
+#        'c': [0.1, 400., 200., 1e-15, 'fF'],
+#        'l': [0, 2000, 0., 1e-12, 'pH'],
+#        'rlo': [0, 1000, 200., 1, 'Ohm'],
+#    }
+    params = {
+        'r': [1., 20000., 1000., 1, 'Ohm'],
+        'c': [0.1, 400., 200., 1e-15, 'fF'],
+        'l': [0, 10000, 0., 1e-12, 'pH'],
+        'rlo': [0, 20000, 200., 1, 'Ohm'],
+    }
+
+    values = {}    # this is where the fitter will store the values
+
+    def __init__(self):
+        self.reset_values()
+
+    def reset_values(self):
+        for p in self.params:
+            self.values[p] = self.params[p][2]*self.params[p][3]
+
+    def admittance(self, w, r, c, l, rlo):
+        """Admittance of a Field Effect Capacitor
+
+        Parameters
+        ----------
+        w : float or np.array
+            Pulsation at which to compute the admitance
+
+        r : float or np.array
+            Resistance of the R-C-L line
+
+        c : float or np.array
+            Capacitance of the R-C-L line
+
+        l : float or np.array
+            Inductance of the R-C-L line
+
+        rlo : float or np.array
+            Low frequency resistance = r/3+ra
+
+        Returns
+        -------
+        float : computed admittance
+
+        """
+        r = r + 1j*l*w
+        k = np.sqrt(-1j*r*c*w)
+        tlm = (1j*k/r) * np.tanh(1j*k)
+        adm = 1./(1./tlm + rlo - r/3)
+        return adm
+
+    def objective(self, params, x_data, y_data):
+        """Error function function minimized during the fitting procedure.
+
+        We perform the optimization on both the real and imaginary part.
+        """
+        r, c, l, rlo = (params['r'].value, params['c'].value,
+                        params['l'].value, params['rlo'].value) 
+        computed_admittance = self.admittance(x_data, r, c, l, rlo)
+        np.nan_to_num(computed_admittance)
+        res = np.empty((2, len(y_data)))
+        res[0] = y_data.real - computed_admittance.real
+        res[1] = y_data.imag - computed_admittance.imag
+        return np.ravel(res)
+
+    def fit_RCRa(self, base_f, base_y):
+        # define initial values
+        self.values['r'] = 200.
+        self.values['c'] = 200e-15        
+        self.values['l'] = 0.
+        self.values['rlo'] = 230.
+        
+        # get crossover frequency
+        # avoid detecting the crossover associated with the leak or associated to high frequency weirdness
+        mask = np.logical_and(base_f > 2e8, base_f < 5e9)
+        # try to detect crossover using threshold
+        f_below_thresh = base_f[mask][np.abs(base_y[mask].real-base_y[mask].imag)<5e-5]
+        if len(f_below_thresh):
+            fc = f_below_thresh[0]
+        else:
+            print "Threshold detection did not work"
+            fc = base_f[mask][np.argmin(np.abs(base_y[mask].real-base_y[mask].imag))]
+        
+        print "Detected fc:", fc/1e9, "GHz"
+        #if fc < 3.1e8: fc = 1e9
+        
+        # define masks
+        masks = [base_f < fc/5.]    # here we will only fit C
+        masks += [base_f < 2.*fc]   # here we will only fit Rlo
+        
+        # do it again
+        masks += [base_f < fc/5]
+        masks += [base_f < 2.*fc]
+        
+        # now fit R
+        masks += [base_f < 5.*fc]
+        
+        # Rlo again
+        masks += [base_f < 2.*fc]
+        
+        # L
+        #masks += [np.logical_and(base_f > 2e9, base_f < 15e9)]
+
+        # fit
+        for i, mask in enumerate(masks):
+            w = 2.*np.pi*base_f[mask]
+            y = base_y[mask]  # minus sign because Y12 and not Y11
+
+            # create fit parameters
+            params = Parameters()
+            params.add('c', value=self.values['c'], min=self.params['c'][0]*self.params['c'][3], max=self.params['c'][1]*self.params['c'][3], vary=True if i in [0, 2] else False)
+            params.add('rlo', value=self.values['rlo'], min=self.params['rlo'][0]*self.params['rlo'][3], max=self.params['rlo'][1]*self.params['rlo'][3], vary=True if i in [1, 3, 5] else False)
+            params.add('r', value=self.values['r'], min=self.params['r'][0]*self.params['r'][3], max=self.params['r'][1]*self.params['r'][3], vary=True if i in [4,6] else False)
+            params.add('l', value=self.values['l'], min=self.params['l'][0]*self.params['l'][3], max=self.params['l'][1]*self.params['l'][3], vary=True if i in [6] else False)
+        
+            # execute fit
+            res = minimize(self.objective, params, args=(w, y))
+            for p in self.values:
+                self.values[p] = res.params[p].value
