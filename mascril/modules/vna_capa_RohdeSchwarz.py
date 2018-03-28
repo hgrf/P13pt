99597af137326f689ee498db1da92730ebcc1b30
green-mercury <holger.graef@gmail.com>
2018-03-28 13:40:45 +0200

    Tidied up modules

--- mascril/modules/vna_capa_RohdeSchwarz.py
+++ mascril/modules/vna_capa_RohdeSchwarz.py
@@ -1,19 +1,25 @@
-from P13pt.mascril.measurement import MeasurementBase
+'''
+This script uses the following driver for the Rohde and Schwarz VNA:
+    https://github.com/Terrabits/rohdeschwarz
+'''
+
+from P13pt.mascril.measurement import MeasurementBase, Sweep, String, Folder, Boolean
 from P13pt.drivers.bilt import Bilt, BiltVoltageSource, BiltVoltMeter
-from P13pt.drivers.anritsuvna import AnritsuVNA
+from rohdeschwarz.instruments.vna import Vna as RohdeSchwarzVNA
 
 import time
-import numpy as np
 import os
 import errno
 
 class Measurement(MeasurementBase):
     params = {
-        'Vgs': [0.01],
+        'Vgs': Sweep([0.]),
         'Rg': 100e3,
         'stabilise_time': 0.3,
-        'comment': None,
-        'data_dir': r'D:\MeasurementJANIS\Holger\PRC-TopGate-D3\2018.01.18_VNA_RT'
+        'comment': String(''),
+        'data_dir': Folder(r'D:\MeasurementJANIS\Holger\test'),
+        'use_vna': Boolean(True),
+        'init_bilt': Boolean(False)
     }
 
     observables = ['Vg', 'Vgm', 'Ileak']
@@ -22,7 +28,7 @@ class Measurement(MeasurementBase):
         ['np.abs(Ileak) > 1e-8', MeasurementBase.ALARM_CALLCOPS]
     ]
 
-    def measure(self, data_dir, Vgs, Rg, comment, stabilise_time, **kwargs):
+    def measure(self, data_dir, Vgs, Rg, comment, stabilise_time, use_vna, init_bilt, **kwargs):
         print "==================================="        
         print "Starting acquisition script..."
 
@@ -30,8 +36,11 @@ class Measurement(MeasurementBase):
         try:
             print "Setting up DC sources and voltmeters..."
             bilt = Bilt('TCPIP0::192.168.0.2::5025::SOCKET')
-            # source (bilt, channel, range, filter, slope in V/ms, label):
-            self.sourceVg = sourceVg = BiltVoltageSource(bilt, "I1", "12", "1", 0.005, "Vg")
+            if init_bilt:
+                # source (bilt, channel, range, filter, slope in V/ms, label):
+                self.sourceVg = sourceVg = BiltVoltageSource(bilt, "I1", "12", "1", 0.005, "Vg")
+            else:
+                self.sourceVg = sourceVg = BiltVoltageSource(bilt, "I1", initialise=False)
             # voltmeter (bilt, channel, filt, label=None)
             self.meterVg = meterVg = BiltVoltMeter(bilt, "I5;C1", "2", "Vgm")
             print "DC sources and voltmeters are set up."
@@ -41,8 +50,8 @@ class Measurement(MeasurementBase):
             
         try:
             print "Setting up VNA"
-            vna = AnritsuVNA('GPIB::6::INSTR')
-            self.freqs = vna.get_freq_list()         # get frequency list
+            self.vna = vna = RohdeSchwarzVNA()
+            vna.open('GPIB', '7')
             print "VNA is set up."
         except:
             print "There has been an error setting up the VNA."
@@ -54,13 +63,14 @@ class Measurement(MeasurementBase):
         filename = timestamp + ('_'+comment if comment else '')
         self.prepare_saving(os.path.join(data_dir, filename+'.txt'))
 
-        # prepare saving RF data
-        spectra_fol = os.path.join(data_dir, filename)
-        try:
-            os.makedirs(spectra_fol)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
+        if use_vna:
+            # prepare saving RF data
+            spectra_fol = os.path.join(data_dir, filename)
+            try:
+                os.makedirs(spectra_fol)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
 
         for Vg in Vgs:
             if self.flags['quit_requested']:
@@ -83,20 +93,20 @@ class Measurement(MeasurementBase):
     
             # save DC data
             self.save_row(locals())
-        
-            # save VNA data
-            print "Getting VNA spectra..."
-            vna.single_sweep()
-            table = vna.get_table([1,2,3,4])
-            timestamp = time.strftime('%Y-%m-%d_%Hh%Mm%Ss')  
-            spectrum_file = timestamp+'_Vg=%2.4f'%(Vg)+'.txt'
-            np.savetxt(os.path.join(spectra_fol, spectrum_file), np.transpose(table))
+
+            if use_vna:
+                # save VNA data
+                print "Getting VNA spectra|"
+                timestamp = time.strftime('%Y-%m-%d_%Hh%Mm%Ss')
+                spectrum_file = timestamp+'_Vg=%2.4f'%(Vg)
+                vna.channel(1).save_measurement_locally(os.path.join(spectra_fol, spectrum_file), (1,2))
 
         print "Acquisition done."
         
         return locals()
 
     def tidy_up(self):
+        self.vna.close()
         self.end_saving()
         print "Driving all voltages back to zero..."
         self.sourceVg.set_voltage(0.)
