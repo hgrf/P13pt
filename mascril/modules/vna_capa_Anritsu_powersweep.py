6e7511e9d79b435603b4309f83cc6245b9b28fce
green-mercury <holger.graef@gmail.com>
2018-05-30 20:05:31 +0200

    MAScriL: added module for VNA power sweep

--- mascril/modules/vna_capa_Anritsu_powersweep.py
+++ mascril/modules/vna_capa_Anritsu_powersweep.py
@@ -12,8 +12,10 @@ import errno
 class Measurement(MeasurementBase):
     params = {
         'Vgs': Sweep([0.]),
+        'pwrs': Sweep([0.]),
         'Rg': 100e3,
-        'stabilise_time': 0.3,
+        'Vg_stabilise_time': 5.,
+        'pwr_stabilise_time': 5.,
         'comment': String(''),
         'data_dir': Folder(r''),
         'use_vna': Boolean(True),
@@ -26,7 +28,8 @@ class Measurement(MeasurementBase):
         ['np.abs(Ileak) > 1e-8', MeasurementBase.ALARM_CALLCOPS]
     ]
 
-    def measure(self, data_dir, Vgs, Rg, comment, stabilise_time, use_vna, init_bilt, **kwargs):
+    def measure(self, data_dir, Vgs, pwrs, Rg, comment, Vg_stabilise_time,
+                pwr_stabilise_time, use_vna, init_bilt, **kwargs):
         print "==================================="        
         print "Starting acquisition script..."
 
@@ -50,6 +53,13 @@ class Measurement(MeasurementBase):
             print "Setting up VNA..."
             vna = AnritsuVNA('GPIB::6::INSTR')
             sweeptime = vna.get_sweep_time()
+            source_att = vna.get_source_att(1)
+            source_att2 = vna.get_source_att(2)
+            if not source_att == source_att2:
+                print "This module should only be used when the two sources use the same attenuator."
+                raise Exception
+            print "Detected same source attenuator on ports 1 and 2:"
+            print source_att, "dB"
             print "VNA is set up."
         except:
             print "There has been an error setting up the VNA."
@@ -69,22 +79,21 @@ class Measurement(MeasurementBase):
             except OSError as e:
                 if e.errno != errno.EEXIST:
                     raise
-            if vna.get_sweep_type() == 'FSEGM':
-                with open(os.path.join(spectra_fol, 'VNAconfig'), 'w') as f:
-                    vna.dump_freq_segments(f)
+            if vna.get_sweep_type() != 'FSEGM':
+                print "This module should only be used when the VNA is in segmented sweep mode."
+                raise Exception
+#            if vna.get_sweep_type() == 'FSEGM':
+#                with open(os.path.join(spectra_fol, 'VNAconfig'), 'w') as f:
+#                    vna.dump_freq_segments(f)
 
-        for Vg in Vgs:
-            if self.flags['quit_requested']:
-                print "Stopping acquisition."
-                return locals()            
-            
+        for Vg in Vgs:           
             print "Setting Vg = {}".format(Vg)
         
             # set Vg
             sourceVg.set_voltage(Vg)
             
             # wait
-            time.sleep(stabilise_time)
+            time.sleep(Vg_stabilise_time)
         
             # read voltages
             Vgm = meterVg.get_voltage()
@@ -96,18 +105,36 @@ class Measurement(MeasurementBase):
             self.save_row(locals())
 
             if use_vna:
-                # save VNA data
-                print "Getting VNA spectra..."
-                vna.single_sweep(wait=False)
-                # display sweep progress
-                progressbar_wait(sweeptime)
-                # make sure sweep is really done
-                while not vna.is_sweep_done():
-                    time.sleep(0.5)
-                table = vna.get_table([1,2,3,4])
-                timestamp = time.strftime('%Y-%m-%d_%Hh%Mm%Ss')
-                spectrum_file = timestamp+'_Vg=%2.4f'%(Vg)+'.txt'
-                np.savetxt(os.path.join(spectra_fol, spectrum_file), np.transpose(table))
+                # sweep power
+                for pwr in pwrs:
+                    if self.flags['quit_requested']:
+                        print "Stopping acquisition."
+                        return locals()    
+                    
+                    print "Setting pwr = {} dBm".format(pwr)
+                    
+                    # set power on VNA for all frequency segments
+                    pwr_to_set = pwr+source_att
+                    segm_count = int(vna.query(':SENS:FSEGM:COUN?'))
+                    for i in range(1,segm_count+1):
+                        vna.write(":SENS:FSEGM{}:POW:PORT1 {}".format(i, pwr_to_set))
+                        vna.write(":SENS:FSEGM{}:POW:PORT2 {}".format(i, pwr_to_set))
+                    
+                    # wait
+                    time.sleep(pwr_stabilise_time)                    
+                    
+                    # save VNA data
+                    print "Getting VNA spectra..."
+                    vna.single_sweep(wait=False)
+                    # display sweep progress
+                    progressbar_wait(sweeptime)
+                    # make sure sweep is really done
+                    while not vna.is_sweep_done():
+                        time.sleep(0.5)
+                    table = vna.get_table([1,2,3,4])
+                    timestamp = time.strftime('%Y-%m-%d_%Hh%Mm%Ss')
+                    spectrum_file = timestamp+'_Vg=%2.4f'%(Vg)+'_pwr='+str(pwr)+'.txt'
+                    np.savetxt(os.path.join(spectra_fol, spectrum_file), np.transpose(table))
 
         print "Acquisition done."
         
