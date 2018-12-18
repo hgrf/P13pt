d68abcc8f4a5a088144dc54d79df10e18f359d0b
Holger Graef <holger.graef@lpa.ens.fr>
2018-12-18 19:35:06 +0100

    added pywin32 dependency to conda convert in conda_deploy.sh

--- conda_build.sh
+++ conda_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-conda build --python 2.7 conda
-conda build --python 3.6 conda
-conda build --python 3.7 conda
+for py_ver in 2.7 3.6 3.7
+do
+    conda build --python $py_ver conda
+done
--- conda_deploy.sh
+++ conda_deploy.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 
-# TODO: do this with for-loop
-PKG_FILE="$(conda build --python 2.7 conda --output)"
-PKG_DIR=$(dirname "${PKG_FILE}")
-CONDA_BLD_DIR=$(dirname "${PKG_DIR}")
-PKG_BASENAME=$(basename "${PKG_FILE}")
-
-for PLATFORM in linux-32 linux-64 win-32 win-64 osx-64
+for py_ver in 2.7 3.6 3.7
 do
-    conda convert --platform $PLATFORM $PKG_FILE -o $CONDA_BLD_DIR
-    anaconda upload $CONDA_BLD_DIR/$PLATFORM/$PKG_BASENAME
+    PKG_FILE="$(conda build --python ${py_ver} conda --output)"
+    PKG_DIR=$(dirname "${PKG_FILE}")
+    CONDA_BLD_DIR=$(dirname "${PKG_DIR}")
+    PKG_BASENAME=$(basename "${PKG_FILE}")
+
+    for PLATFORM in linux-32 linux-64 osx-64
+    do
+        conda convert --platform $PLATFORM $PKG_FILE -o $CONDA_BLD_DIR
+        anaconda upload $CONDA_BLD_DIR/$PLATFORM/$PKG_BASENAME
+    done
+
+    for PLATFORM in win-32 win-64
+    do
+        conda convert --platform $PLATFORM --dependencies pywin32 $PKG_FILE -o $CONDA_BLD_DIR
+        anaconda upload $CONDA_BLD_DIR/$PLATFORM/$PKG_BASENAME
+    done
+
 done
-
-PKG_FILE="$(conda build --python 3.6 conda --output)"
-PKG_DIR=$(dirname "${PKG_FILE}")
-CONDA_BLD_DIR=$(dirname "${PKG_DIR}")
-PKG_BASENAME=$(basename "${PKG_FILE}")
-
-for PLATFORM in linux-32 linux-64 win-32 win-64 osx-64
-do
-    conda convert --platform $PLATFORM $PKG_FILE -o $CONDA_BLD_DIR
-    anaconda upload $CONDA_BLD_DIR/$PLATFORM/$PKG_BASENAME
-done
-
-PKG_FILE="$(conda build --python 3.7 conda --output)"
-PKG_DIR=$(dirname "${PKG_FILE}")
-CONDA_BLD_DIR=$(dirname "${PKG_DIR}")
-PKG_BASENAME=$(basename "${PKG_FILE}")
-
-for PLATFORM in linux-32 linux-64 win-32 win-64 osx-64
-do
-    conda convert --platform $PLATFORM $PKG_FILE -o $CONDA_BLD_DIR
-    anaconda upload $CONDA_BLD_DIR/$PLATFORM/$PKG_BASENAME
-done
-
