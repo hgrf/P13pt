699d67324d97b605f37eb74bb63a03d467424d66
Holger Graef <holger.graef@gmail.com>
2019-10-21 13:15:43 +0200

    added averaging and speed config in K2400 driver

--- P13pt/drivers/keithley2400.py
+++ P13pt/drivers/keithley2400.py
@@ -14,7 +14,8 @@ from time import sleep
 
 class K2400:
     def __init__(self, connection, sourcemode='V', vrang=None, irang=None,
-                 slope=0.01, initialise=True):
+                 slope=0.01, initialise=True, average=None, average_mode='REP',
+                 speed=10.):
         self.slope = slope
         self.time_step = 0.01     # update voltage every 10 ms when sloping
         
@@ -47,6 +48,16 @@ class K2400:
                 self.write(":SOURce:CURR:RANGe "+str(irang))
             
             self.write(":OUTPut:STATe ON")
+
+        if average is None:
+            self.set_average_state(False)
+        else:
+            self.set_average_state(True)
+            self.set_average_count(average)
+            self.set_filter(average_mode)
+        
+        self.set_speed(speed)       # default value (10) is HI ACCURACY MODE
+        
 
         if not self.query('SYST:ERR?').startswith('0,'):
             raise Exception("Keithley 2400 signals error")
@@ -108,6 +119,54 @@ class K2400:
         self.query(':SENS:FUNC:OFF "VOLT:DC"')
         self.query(':SENS:FUNC:ON "CURR:DC"')
         return float(self.query(':read?').split(',')[1])
+    
+    def get_speed(self):
+        """
+        returns the integration time in terms of power line cycles
+        NB this is a global value for voltage, current and resistance
+        measurements
+        
+        on the instrument you can configure the following values:
+        FAST          0.01
+        MED           0.1
+        NORMAL        1
+        HI ACCURACY   10
+        """
+        return float(self.query(':SENS:VOLT:NPLC?'))
+        
+    
+    def get_filter(self):
+        """
+        returns REP for repeating average and MOV for moving average
+        """
+        return self.query(':SENS:AVER:TCON?')
+    
+    def get_average_count(self):
+        return int(self.query(':SENS:AVER:COUN?'))
+    
+    def get_average_state(self):
+        return bool(int(self.query(':SENS:AVER:STAT?')))
+    
+    def set_speed(self, value):
+        if value < 0.01 or value > 10.:
+            raise Exception('Speed has to be 0.01-10')
+        self.write(':SENS:VOLT:NPLC '+str(value))
+        
+    def set_filter(self, value):
+        value = value.upper()
+        if value not in ['MOV', 'REP']:
+            raise Exception('Filter has to be MOV or REP')
+        self.write(':SENS:AVER:TCON '+value)
+        
+    def set_average_count(self, value):
+        value = int(value)
+        if value < 1 or value > 100:
+            raise Exception('Average count has to be 1-100')
+        self.write(':SENS:AVER:COUN '+str(value))
+        
+    def set_average_state(self, value):
+        value = '1' if value else '0'
+        self.write(':SENS:AVER:STAT '+value)
 
     # just wrapping the main functions of self.k2400
     def query(self, q):
